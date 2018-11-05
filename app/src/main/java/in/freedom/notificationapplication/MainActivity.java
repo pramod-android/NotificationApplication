6246968b417165d74a04a907783de6f7f52211cc
pramod-android <40615535+pramod-android@users.noreply.github.com>
2018-11-05 08:50:02 +0530

    Group Notification

--- app/src/main/java/in/freedom/notificationapplication/MainActivity.java
+++ app/src/main/java/in/freedom/notificationapplication/MainActivity.java
@@ -7,18 +7,25 @@ import android.app.PendingIntent;
 import android.app.TaskStackBuilder;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Build;
+import android.os.SystemClock;
 import android.support.v4.app.NotificationCompat;
 import android.support.v4.app.NotificationManagerCompat;
+import android.support.v4.app.RemoteInput;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static in.freedom.notificationapplication.Notification.CHANNEL_1_ID;
 import static in.freedom.notificationapplication.Notification.CHANNEL_2_ID;
 
 public class MainActivity extends AppCompatActivity {
     private NotificationManagerCompat notificationManager;
+    static List<Message> MESSAGES = new ArrayList<>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,32 +35,111 @@ public class MainActivity extends AppCompatActivity {
 
 
     public void DisplayNotification(View view) {
-        createNotificationChannels();
+        sendOnChannel1();
         sendOnChannel2();
     }
-    private void createNotificationChannels() {
-        Notification notification = new NotificationCompat.Builder(this, CHANNEL_1_ID)
-                .setSmallIcon(R.mipmap.ic_launcher)
-                .setContentTitle("title")
-                .setContentText("message")
-                .setPriority(NotificationCompat.PRIORITY_HIGH)
-                .setCategory(NotificationCompat.CATEGORY_MESSAGE)
+    public void sendOnChannel1() {
+        sendChannel1Notification(this);
+    }
+
+    public static void sendChannel1Notification(Context context) {
+        Intent activityIntent = new Intent(context, MainActivity.class);
+        PendingIntent contentIntent = PendingIntent.getActivity(context,
+                0, activityIntent, 0);
+
+        RemoteInput remoteInput = new RemoteInput.Builder("key_text_reply")
+                .setLabel("Your answer...")
                 .build();
 
+        Intent replyIntent;
+        PendingIntent replyPendingIntent = null;
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+            replyIntent = new Intent(context, HomeActivity.class);
+            replyPendingIntent = PendingIntent.getBroadcast(context,
+                    0, replyIntent, 0);
+        } else {
+            //start chat activity instead (PendingIntent.getActivity)
+            //cancel notification with notificationManagerCompat.cancel(id)
+        }
+
+        NotificationCompat.Action replyAction = new NotificationCompat.Action.Builder(
+                R.mipmap.ic_launcher,
+                "Reply",
+                replyPendingIntent
+        ).addRemoteInput(remoteInput).build();
+
+        NotificationCompat.MessagingStyle messagingStyle =
+                new NotificationCompat.MessagingStyle("Me");
+        messagingStyle.setConversationTitle("Group Chat");
+
+        for (Message chatMessage : MESSAGES) {
+            NotificationCompat.MessagingStyle.Message notificationMessage =
+                    new NotificationCompat.MessagingStyle.Message(
+                            chatMessage.getText(),
+                            chatMessage.getTimestamp(),
+                            chatMessage.getSender()
+                    );
+            messagingStyle.addMessage(notificationMessage);
+        }
+
+        Notification notification = new NotificationCompat.Builder(context, CHANNEL_1_ID)
+                .setSmallIcon(R.mipmap.ic_launcher)
+                .setStyle(messagingStyle)
+                .addAction(replyAction)
+                .setColor(Color.BLUE)
+                .setPriority(NotificationCompat.PRIORITY_HIGH)
+                .setCategory(NotificationCompat.CATEGORY_MESSAGE)
+                .setContentIntent(contentIntent)
+                .setAutoCancel(true)
+                .setOnlyAlertOnce(true)
+                .build();
+
+        NotificationManagerCompat notificationManager = NotificationManagerCompat.from(context);
         notificationManager.notify(1, notification);
     }
 
     public void sendOnChannel2() {
+        String title1 = "Title 1";
+        String message1 = "Message 1";
+        String title2 = "Title 2";
+        String message2 = "Message 2";
 
-
-
-        Notification notification = new NotificationCompat.Builder(this, CHANNEL_2_ID)
+        Notification notification1 = new NotificationCompat.Builder(this, CHANNEL_2_ID)
                 .setSmallIcon(R.mipmap.ic_launcher)
-                .setContentTitle("title")
-                .setContentText("message")
+                .setContentTitle(title1)
+                .setContentText(message1)
                 .setPriority(NotificationCompat.PRIORITY_LOW)
+                .setGroup("example_group")
                 .build();
 
-        notificationManager.notify(2, notification);
+        Notification notification2 = new NotificationCompat.Builder(this, CHANNEL_2_ID)
+                .setSmallIcon(R.mipmap.ic_launcher)
+                .setContentTitle(title2)
+                .setContentText(message2)
+                .setPriority(NotificationCompat.PRIORITY_LOW)
+                .setGroup("example_group")
+                .build();
+
+        Notification summaryNotification = new NotificationCompat.Builder(this, CHANNEL_2_ID)
+                .setSmallIcon(R.mipmap.ic_launcher)
+                .setStyle(new NotificationCompat.InboxStyle()
+                        .addLine(title2 + " " + message2)
+                        .addLine(title1 + " " + message1)
+                        .setBigContentTitle("2 new messages")
+                        .setSummaryText("user@example.com"))
+                .setPriority(NotificationCompat.PRIORITY_LOW)
+                .setGroup("example_group")
+                .setGroupAlertBehavior(NotificationCompat.GROUP_ALERT_CHILDREN)
+                .setGroupSummary(true)
+                .build();
+
+        SystemClock.sleep(2000);
+        notificationManager.notify(2, notification1);
+        SystemClock.sleep(2000);
+        notificationManager.notify(3, notification2);
+        SystemClock.sleep(2000);
+        notificationManager.notify(4, summaryNotification);
     }
 }
+
